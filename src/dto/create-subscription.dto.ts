6eed85cae29d95a0fb67b394836134b68536113b
Berk Akgün <berkakgun@armut.com>
2020-11-12 17:45:55 +0300

    heroku

--- src/dto/create-subscription.dto.ts
+++ src/dto/create-subscription.dto.ts
@@ -1,7 +1,8 @@
+import { CreatePushDto } from "./create-push-dto"
 import { CreateTaskDto } from "./create-task.dto"
 
 export class CreateSubscriptionDto {
-    readonly subscription: CreateSubscriptionDto
+    readonly subscription: CreatePushDto
     readonly timeZone: string
     readonly task: CreateTaskDto[]
 }

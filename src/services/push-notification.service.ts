0f517be0308cedb1d2378e81811b65ea71d7b5fc
Berk Akgün <berkakgun@armut.com>
2020-11-14 23:47:26 +0300

    heroku push notification trial

--- src/services/push-notification.service.ts
+++ src/services/push-notification.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { Day } from 'src/constants';
 
 @Injectable()
 export class PushNotificationService {
 
+    private readonly logger = new Logger('Cron');
+
     constructor(private scheduler: SchedulerRegistry) { }
 
     createNotification(name: string, frequency: string | CronExpression): void {
-        this.isCron(frequency) ?
-            this.scheduler.addCronJob(name, frequency) : null
+
+        //if (this.isCron(frequency)) {
+        this.scheduler.addCronJob(name, frequency)
+        this.logger.warn('cron added');
+        //}
+        this.getCronJobs();
     }
 
     stopNotification(name: string): void {
@@ -20,15 +26,19 @@ export class PushNotificationService {
         this.scheduler.addCronJob(name, `${minute} ${hour} * * ${day}`);
     }
 
-    isCron(cron: string): boolean {
-        let expression: number[];
-        cron.split(' ').map(el => expression.push(parseInt(el, 10)));
-        return expression.length === 5 &&
-            expression[0] < 60 && expression[0] >= 0 &&
-            expression[1] < 24 && expression[1] >= 0 &&
-            expression[2] < 32 && expression[2] >= 0 &&
-            expression[3] < 13 && expression[3] >= 0 &&
-            expression[4] < 7 && expression[4] >= 0
+    getCronJobs() {
+        console.log(this.scheduler.getCronJobs());
     }
+
+    /*     isCron(cron: string): boolean {
+            let expression: number[] = [];
+            cron.split(' ').map(el => expression.push(parseInt(el, 10)));
+            return expression.length === 5 &&
+                expression[0] < 60 && expression[0] >= 0 &&
+                expression[1] < 24 && expression[1] >= 0 &&
+                expression[2] < 32 && expression[2] >= 0 &&
+                expression[3] < 13 && expression[3] >= 0 &&
+                expression[4] < 7 && expression[4] >= 0
+    } */
 
 }
